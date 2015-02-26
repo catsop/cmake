64b1d046a72318a1214a3d7b12183a0090c87394
Andrew Champion <andrew.champion@gmail.com>
2015-02-25 19:46:39 -0500

    Use catsop-specific vigra-git for clang/icc support

--- include/third_party.cmake
+++ include/third_party.cmake
@@ -163,8 +163,8 @@ macro(find_3rd_party name)
       message(STATUS "vigra git version requested -- will download and build it on demand.")
       ExternalProject_Add(
         vigra-git
-        GIT_REPOSITORY http://github.com/ukoethe/vigra.git
-        GIT_TAG 7a492ea284ec3e4f78a48f1a8a939a3d9c749f47
+        GIT_REPOSITORY http://github.com/catsop/vigra.git
+        GIT_TAG 262d07eb7bca28b21bdcf9f971741c946a55fd5b
         UPDATE_COMMAND ""
         PATCH_COMMAND ""
         CMAKE_ARGS -DAUTOBUILD_TESTS:BOOL=OFF -DVIGRA_STATIC_LIB:BOOL=ON -DWITH_VIGRANUMPY:BOOL=OFF
